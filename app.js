6e1bf5cdc128704fca944f0458939c9ebaa69847
Igwe Augustine <augustusbest00@gmail.com>
2018-11-06 10:14:09 -0900

    i refactored the code and use body-parser module properly

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var port = process.env.PORT || 4000;
 var morgan = require('morgan');
 
 //cors {Cross Origin Request}
@@ -17,18 +16,20 @@ app.use((req, res, next) => {
     next();
 });
 
+// Routes
+const ArtisanRoutes = require('./routes/Artisian'); 
+
 
 //middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/client'));
 app.use(morgan('dev'));
 
-Artisan = require('./models/artisan')
-
 
 //connecting to mongoose
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/Artisan', { useNewUrlParser: true })
-var db = mongoose.connection;
 
 
 //route for homepage
@@ -36,33 +37,21 @@ app.get('/', (req, res) => {
     res.send(' Use the /api/artisan endpoint');
 });
 
+app.use('/artisians', ArtisanRoutes);
 
-// add artisan
-app.post('/api/addArtisan', function (req, res) {
-    try {
-        var artisan = req.body;
-        Artisan.addArtisan(artisan, (artisan) => {
-            res.json({ artisan, message: 'Successfully Registered' });
-        });
-    }
-    catch (err) {
-        console.log(err);
-        res.json({ Err: err, message: 'Please Try Again: an error occured during registration' })
-    }
-});
-
-//get all artisans
-app.get('/api/getArtisans', function (req, res) {
-    try {
-        Artisan.getArtisans((err, artisans) => {
-            res.json(artisans);
-        });
-    }
-    catch (err) {
-        console.log(err);
-    }
-});
-
-app.listen(port, function () {
-    console.log('Server listening at ' + port);
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
 })
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+})
+
+module.exports = app;
