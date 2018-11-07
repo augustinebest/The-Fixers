9f4cd3195e20969bf6e221fbe785c14925191567
Igwe Augustine <augustusbest00@gmail.com>
2018-11-07 10:02:00 -0900

    this controller  is working fine

--- Contollers/ArtisianContoller.js
+++ Contollers/ArtisianContoller.js
@@ -1,26 +1,38 @@
-const Artisian = require('../Models/Artisan');
+const Artisan = require('../Models/Artisan')
 const cloud = require('../Services/cloudinary');
 
 exports.addArtisans = (req, res, next) => {
-    console.log(req.file);
-    const artisan = {
+    const artisan = new Artisan({
         name: req.body.name,
         phoneNumber: req.body.phoneNumber,
-        JobDescription: req.body.JobDescription,
+        jobDescription: req.body.JobDescription,
+        imageID: '',
         address: req.body.address,
         location: req.body.location,
         pictures: req.file.path,
-        DateAvailable: req.body.DateAvailable,
-        ExperienceLevel: req.body.ExperienceLevel
-    };
+        dateAvailable: req.body.DateAvailable,
+        experienceLevel: req.body.ExperienceLevel
+    });
     try {
-        cloud.upload(artisan.pictures).then(result => {
-            console.log(result);
-        })
-        console.log(artisan);
+        if(req.body.name == '' || req.body.name == null || req.body.phoneNumber == '' || req.body.phoneNumber == null || req.body.jobDescription == '' || req.body.jobDescription == null  || req.body.address == '' || req.body.address == null || req.body.location == '' || req.body.location == null || req.body.dateAvailable == '' || req.body.dateAvailable == null) {
+            res.json({message: 'The field(s) are required', code: 9});
+        } else {
+            cloud.upload(artisan.pictures).then(result => {
+                artisan.pictures = result.url;
+                artisan.imageID = result.Id;
+                console.log(artisan);
+                Artisan.create(artisan, (err, result) => {
+                    if(err) res.json({message: 'error ocurred in adding this artisan', code: 10});
+                    if(result) {
+                        res.json({message: 'this artisan have been created', code: 11})
+                        console.log(artisan);
+                    }
+                })
+            })
+        }
     }
     catch (err) {
-        res.json({err: err, message: 'Please Try Again: an error occured during registration' })
+        res.json({err: err, message: 'Please Try Again: an error occured during registration', code: 12 })
     }
 }
 
