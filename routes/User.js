ed94925cdfa4f078b0b0279ee678c6f1f9442033
Igwe Augustine <augustusbest00@gmail.com>
2018-11-08 01:17:36 -0900

    the login function

--- routes/User.js
+++ routes/User.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require('../Contollers/UserController');
 
 router.post('/signup', userController.signup);
-// router.post('/login', userController.login);
+router.post('/login', userController.login);
 
 module.exports = router;
