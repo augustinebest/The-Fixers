02138b778e10a8879ed4f56faab3687fca1b6b4b
Igwe Augustine <augustusbest00@gmail.com>
2018-11-07 23:59:39 -0900

    commented out previous changes made

--- Contollers/UserController.js
+++ Contollers/UserController.js
@@ -60,6 +60,6 @@ exports.signup = (req, res, next) => {
     }
 }
 
-exports.login = (req, res, next) => {
-    res.json('Yay');
-}
+// exports.login = (req, res, next) => {
+//     res.json('Yay');
+// }
